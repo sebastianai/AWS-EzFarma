f4ba463576f9901504796f4c7170b16bed16a7de
sebastian mora <deoxides@gmail.com>
2022-06-15 16:43:10 -0400

    carrito terminado

--- scripts/views.sql
+++ scripts/views.sql
@@ -1,3 +1,4 @@
+/*Productos marketplace*/
 CREATE OR REPLACE VIEW MARKET_Producto AS
-SELECT * FROM Catalogo_Producto cp JOIN Catalogos c ON cp.ID_CATALOGO = c.ID
+SELECT cp.*,c.NOMBRE FROM Catalogo_Producto cp JOIN Catalogos c ON cp.ID_CATALOGO = c.ID
 WHERE c.VIGENTE = True;
